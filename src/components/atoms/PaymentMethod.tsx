07e93c21524e87b419abd908fc041c42b5c4f043
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-23 16:47:01 +0200

    refactor: change border radius

--- src/components/atoms/PaymentMethod.tsx
+++ src/components/atoms/PaymentMethod.tsx
@@ -4,7 +4,7 @@ type PaymentMethod = {
 
 export const PaymentMethod = ({text}: PaymentMethod) => {
     return(
-        <div className="flex w-full py-4 px-2 items-center gap-2 rounded-md bg-black hover:bg-slate-900 text-base leading-6 font-medium text-white">
+        <div className="flex w-full py-4 px-2 items-center gap-2 rounded bg-black hover:bg-slate-900 text-base leading-6 font-medium text-white">
             {text}
         </div>
     )
