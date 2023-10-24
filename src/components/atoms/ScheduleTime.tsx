5cae262fade76063033503ca8eee19fc5e3de731
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-24 22:06:33 +0200

    refactor: change width

--- src/components/atoms/ScheduleTime.tsx
+++ src/components/atoms/ScheduleTime.tsx
@@ -1,11 +1,11 @@
-type ScheduleTime = {
+export type ScheduleTime = {
     startTime : string,
     endTime: string
 }
 
 export const ScheduleTime = ({startTime, endTime}: ScheduleTime) => {
     return(
-        <div className="flex py-4 px-2 items-center justify-center gap-2 rounded bg-black hover:bg-slate-800">
+        <div className="flex py-4 px-2 items-center justify-center gap-2 rounded bg-black hover:bg-slate-800 w-full">
             <span className="text-base leading-6 font-normal text-white">{startTime} - {endTime}</span>
         </div>
     )
