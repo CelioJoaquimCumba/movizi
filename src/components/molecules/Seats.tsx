80624d9ed59746d94735c468516fc9e70926564a
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-25 00:26:05 +0200

    feat: add seats component

---
+++ src/components/molecules/Seats.tsx
@@ -0,0 +1,42 @@
+import { Seat } from "../atoms/Seat"
+
+type Seats = {
+    row: number,
+    column: number,
+    start: number,
+    sold: Array<{row: number, column: number}>
+}
+
+export const Seats = ({row, column, start, sold}: Seats) => {
+    const matrix: Array<Array<number>> = []
+    for (let i = 0 ; i < row ; i ++ ) {
+        const array = []
+        for (let j = 0 ; j < column ; j ++ ) {
+            if ( sold && sold.some(obj => obj.row -1 === j && obj.column-1 === i)) {
+                array.push(0)
+            } else {
+                array.push(j+start)
+            }
+        }
+        matrix.push(array)
+    }
+    console.log(sold.some(obj => obj.row === 1 && obj.column === 2))
+    return(
+        <div className="flex flex-col gap-1 items-start">
+            {
+            matrix.map(
+                (array, index)=>{
+                    return(
+                        <div key={index} className="flex flex-start gap-1">
+                            {array.map(
+                                (seat)=> 
+                                <Seat key={seat} seat={seat.toString()} status={ seat === 0 ? "occupied" : "available"} />
+                            )}
+                        </div>
+                    )
+                }
+            )
+            }
+        </div>
+    )
+}
