ee1da5f1db6c885280bd7384b0f9cc3ee6039d36
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-24 11:57:25 +0200

    refactor: change input outline

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -8,9 +8,9 @@ type Input = {
 } & React.HTMLProps<HTMLInputElement>
 const Input = ({label, placeholder,...props}: Input) => {
     return (
-        <div className='flex flex-col items-start gap-1 rounded'>
+        <div className='flex flex-col items-start gap-1 rounded w-full'>
             <label className='text-sm leading-5 font-normal text-white'>{label}</label>
-            <input placeholder={placeholder} {...props} className='flex py-4 px-2 justify-between items-center self-stretch rounded bg-black hover:bg-slate-900 text-sm leading-5 font-normal text-white placeholder-gray-400' />
+            <input placeholder={placeholder} {...props} className='flex py-4 px-2 justify-between items-center self-stretch rounded bg-black hover:bg-slate-900 text-sm leading-5 font-normal text-white placeholder-gray-400 border-slate-700 focus:outline-1 border-[1px]' />
         </div>
     )
 }
