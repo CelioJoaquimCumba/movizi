65924e1d2d671888bf619a2d3a01808acf472054
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-24 22:09:49 +0200

    refactor: change alignment

--- src/components/atoms/ScheduleTime.tsx
+++ src/components/atoms/ScheduleTime.tsx
@@ -5,7 +5,7 @@ export type ScheduleTime = {
 
 export const ScheduleTime = ({startTime, endTime}: ScheduleTime) => {
     return(
-        <div className="flex py-4 px-2 items-center justify-center gap-2 rounded bg-black hover:bg-slate-800 w-full">
+        <div className="flex py-4 px-2 items-center gap-2 rounded bg-black hover:bg-slate-800 w-full">
             <span className="text-base leading-6 font-normal text-white">{startTime} - {endTime}</span>
         </div>
     )
