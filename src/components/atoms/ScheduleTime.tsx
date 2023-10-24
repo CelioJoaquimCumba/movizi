cc09b1f7445d45f3f570da03cba2a424d66febfd
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-24 23:33:03 +0200

    refactor: add an onclick event

--- src/components/atoms/ScheduleTime.tsx
+++ src/components/atoms/ScheduleTime.tsx
@@ -1,11 +1,15 @@
 export type ScheduleTime = {
     startTime : string,
     endTime: string
+    onClick: () => void
 }
 
-export const ScheduleTime = ({startTime, endTime}: ScheduleTime) => {
+export const ScheduleTime = ({startTime, endTime, onClick}: ScheduleTime) => {
+    const handleClick = () => {
+        onClick()
+    }
     return(
-        <div className="flex py-4 px-2 items-center gap-2 rounded bg-black hover:bg-slate-800 w-full">
+        <div className="flex py-4 px-2 items-center gap-2 rounded bg-black hover:bg-slate-800 w-full" onClick={handleClick}>
             <span className="text-base leading-6 font-normal text-white">{startTime} - {endTime}</span>
         </div>
     )
