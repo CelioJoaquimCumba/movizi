2c1f3d4b88f22076994da37811a487323d0bf1ca
CelioJoaquimCumba <ccumba82@gmail.com>
2023-10-25 05:21:54 -0700

    refactor: allow to close when the day is selected

--- src/components/molecules/DatePicker.tsx
+++ src/components/molecules/DatePicker.tsx
@@ -45,14 +45,15 @@ export const DatePicker = () => {
     }
     const handleSelect = (day: string) => {
         setSelectedDate(`${day}-${monthsOfYear[month]}-${year}`)
+        handleOpen()
     }
     const handleOpen = () => {
         setIsOpen(isOpen => !isOpen)
     }
 
     return(
-        <div className={`flex py-4 px-2 flex-col items-center gap-2 rounded border-2 border-slate-700 bg-black ${ !isOpen && "hover:bg-slate-800"} w-full`} onClick={handleOpen}>
-            <div className="flex justify-content items-center gap-2 self-stretch">
+        <div className={`flex py-4 px-2 flex-col items-center gap-2 rounded border-2 border-slate-700 bg-black ${ !isOpen && "hover:bg-slate-800"} w-full`} >
+            <div className="flex justify-content items-center gap-2 self-stretch" onClick={handleOpen}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" >
                     <path d="M8 7V3M16 7V3M7 11H17M5 21H19C19.5304 21 20.0391 20.7893 20.4142 20.4142C20.7893 20.0391 21 19.5304 21 19V7C21 6.46957 20.7893 5.96086 20.4142 5.58579C20.0391 5.21071 19.5304 5 19 5H5C4.46957 5 3.96086 5.21071 3.58579 5.58579C3.21071 5.96086 3 6.46957 3 7V19C3 19.5304 3.21071 20.0391 3.58579 20.4142C3.96086 20.7893 4.46957 21 5 21Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
