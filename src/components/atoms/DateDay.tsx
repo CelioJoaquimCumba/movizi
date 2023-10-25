558b7ab419bffae12511aa5a93ba2b6827c40e5c
CelioJoaquimCumba <ccumba82@gmail.com>
2023-10-25 05:34:15 -0700

    feat: add disabled option

--- src/components/atoms/DateDay.tsx
+++ src/components/atoms/DateDay.tsx
@@ -1,14 +1,15 @@
 type DateDay = {
     day: string,
     onClick?: (day:string)=>void
+    disabled?: boolean
 }
-export const DateDay = ({day, onClick}: DateDay) => {
+export const DateDay = ({day, onClick, disabled =false}: DateDay) => {
     const handleClick = () => {
-        onClick && onClick(day)
+        !disabled && onClick && onClick(day)
     }
     return(
-        <div onClick={handleClick} className="flex md:w-10 md:h-10 w-8 h-8 p-1 flex-col justify-center items-center gap-2 flex-shrink-0 rounded border-gray-800 bg-slate-900 hover:border-none hover:bg-slate-800 active:bg-slate-900 hover:cursor-pointer">
-            <span className="text-base leading-6 font-normal text-white">{day}</span>
+        <div onClick={handleClick} className={`flex md:w-10 md:h-10 w-8 h-8 p-1 flex-col justify-center items-center gap-2 flex-shrink-0 rounded border-gray-800 ${!disabled ? "bg-slate-900 hover:bg-slate-800 active:bg-slate-900" : "bg-gray-300"}  hover:border-none  hover:cursor-pointer`}>
+            <span className={`text-base leading-6 font-normal ${!disabled ? "text-white" : "text-gray-200"}`}>{day}</span>
         </div>
     )
 }
