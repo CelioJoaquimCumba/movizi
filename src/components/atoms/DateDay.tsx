0041aaae2d5b63567295dd78d6592083ea224945
CelioJoaquimCumba <ccumba82@gmail.com>
2023-10-25 03:56:25 -0700

    refactor: change cursor pointer

--- src/components/atoms/DateDay.tsx
+++ src/components/atoms/DateDay.tsx
@@ -3,7 +3,7 @@ type DateDay = {
 }
 export const DateDay = ({day}: DateDay) => {
     return(
-        <div className="flex w-10 h-10 p-1 flex-col justify-center items-center gap-2 flex-shrink-0 rounded border-gray-800 bg-slate-900 hover:border-none hover:bg-slate-800">
+        <div className="flex w-10 h-10 p-1 flex-col justify-center items-center gap-2 flex-shrink-0 rounded border-gray-800 bg-slate-900 hover:border-none hover:bg-slate-800 hover:cursor-pointer">
             <span className="text-base leading-6 font-normal text-white">{day}</span>
         </div>
     )
