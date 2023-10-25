8357fc6ee4303f7c8d78a1fbb0a65b5b7065027c
CelioJoaquimCumba <ccumba82@gmail.com>
2023-10-25 05:14:15 -0700

    refactor: add onClick

--- src/components/atoms/DateDay.tsx
+++ src/components/atoms/DateDay.tsx
@@ -1,9 +1,13 @@
 type DateDay = {
-    day: string
+    day: string,
+    onClick?: (day:string)=>void
 }
-export const DateDay = ({day}: DateDay) => {
+export const DateDay = ({day, onClick}: DateDay) => {
+    const handleClick = () => {
+        onClick && onClick(day)
+    }
     return(
-        <div className="flex w-10 h-10 p-1 flex-col justify-center items-center gap-2 flex-shrink-0 rounded border-gray-800 bg-slate-900 hover:border-none hover:bg-slate-800 hover:cursor-pointer">
+        <div onClick={handleClick} className="flex md:w-10 md:h-10 w-8 h-8 p-1 flex-col justify-center items-center gap-2 flex-shrink-0 rounded border-gray-800 bg-slate-900 hover:border-none hover:bg-slate-800 active:bg-slate-900 hover:cursor-pointer">
             <span className="text-base leading-6 font-normal text-white">{day}</span>
         </div>
     )
