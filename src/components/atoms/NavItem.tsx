9b32b0c9e7dd8d2b8900bc97c14e454dc80a3896
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-25 20:27:44 +0200

    refactor: add cursor pointer

--- src/components/atoms/NavItem.tsx
+++ src/components/atoms/NavItem.tsx
@@ -4,7 +4,7 @@ type NavItem = {
 }
 export const NavItem = ({text}: NavItem) => {
     return (
-        <div className="flex py-1 px-2 justify-center items-center gap-2 rounded bg-opacity-0 bg-white hover:bg-opacity-25 active:bg-opacity-100 text-white active:text-slate-800">
+        <div className="flex py-1 px-2 justify-center items-center gap-2 rounded bg-opacity-0 bg-white hover:bg-opacity-25 active:bg-opacity-100 text-white active:text-slate-800 cursor-pointer">
             <span className="text-sm leading-5 font-normal">
                 {text}
             </span>
