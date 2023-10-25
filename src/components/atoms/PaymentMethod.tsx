a635ece83ba10a7afd1073168e38839ca2b8c9d3
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-25 20:26:54 +0200

    refactor: add onClick

--- src/components/atoms/PaymentMethod.tsx
+++ src/components/atoms/PaymentMethod.tsx
@@ -1,10 +1,14 @@
 type PaymentMethod = {
     text: string
+    onClick?: () => void
 }
 
-export const PaymentMethod = ({text}: PaymentMethod) => {
+export const PaymentMethod = ({text, onClick}: PaymentMethod) => {
+    const handleClick = () => {
+        onClick && onClick()
+    }
     return(
-        <div className="flex w-full py-4 px-2 items-center gap-2 rounded bg-black hover:bg-slate-900 text-base leading-6 font-medium text-white">
+        <div onClick={handleClick} className="flex w-full py-4 px-2 items-center gap-2 rounded bg-black hover:bg-slate-900 text-base leading-6 font-medium text-white">
             {text}
         </div>
     )
