6241bb045b0688cae2f3f59981c793e54bd9d610
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-26 22:11:47 +0200

    refactor: add hover effect

--- src/components/molecules/CardMovie.tsx
+++ src/components/molecules/CardMovie.tsx
@@ -9,7 +9,7 @@ export const CardMovie = ({image, title, genre, duration, language, ranking, ori
     return(
         <div style={{ backgroundImage: `url(${type === "default" || orientation === "vertical" ? image.portrait: image.landscape}})`, backgroundSize: "cover" }} className={` ${type === "default" || orientation === "vertical" ? "aspect-[3/4]" : "aspect-[4/2]"} rounded-lg h-72 ${type === "rank" && "h-full"} flex flex-col justify-end hover:cursor-pointer w-full`}>
                 { type === "default" ?
-                    <div className={`flex h-full rounded-lg  p-1 flex-col justify-end items-center gap-2 flex-shrink-0  ${type === "default" && "bg-gradient-to-t from-black hover:from-slate-900 to-transparent"}`}>
+                    <div className={`flex h-full rounded-lg  p-1 flex-col justify-end items-center gap-2 flex-shrink-0  bg-gradient-to-t from-black hover:from-transparent to-transparent`}>
                         <div className="flex flex-col justify-end items-center gap-2 self-stretch">
                             <span className="text-sm leading-5 font-semibold text-white text-center">{title}</span>
                             <span className="text-xs leading-4 font-normal text-white text-center">{genre} | {duration} | {language}</span>
@@ -17,10 +17,12 @@ export const CardMovie = ({image, title, genre, duration, language, ranking, ori
                         </div>
                     </div>
                 :
-                <div className={`flex h-fit p-2 ${ orientation === "vertical" ? "flex-col" : "flex-row"} justify-end items-start gap-2 self-stretch rounded-b-lg bg-black bg-opacity-75`}>
-                    <span className="text-2xl leading-8 font-black text-white">{ranking}</span>
-                    <div className={`${ orientation === "vertical" ? "w-8 h-1" : "w-1 h-8"} border-2 border-white`}></div>
-                    <span className="text-lg leading-7 font-semibold w-full text-white flex-grow">{title}</span>
+                <div className="flex flex-col justify-end h-full w-full bg-gradient-to-t from-slate-900/50 hover:from-transparent to-transparent`">
+                    <div className={`flex h-fit p-2 ${ orientation === "vertical" ? "flex-col" : "flex-row"} justify-end items-start gap-2 self-stretch rounded-b-lg bg-black bg-opacity-75 `}>
+                        <span className="text-2xl leading-8 font-black text-white">{ranking}</span>
+                        <div className={`${ orientation === "vertical" ? "w-8 h-1" : "w-1 h-8"} border-2 border-white`}></div>
+                        <span className="text-lg leading-7 font-semibold w-full text-white flex-grow">{title}</span>
+                    </div>
                 </div>
                 }
         </div>
