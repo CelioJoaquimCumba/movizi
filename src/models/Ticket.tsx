e265218ac5693e30465a40cd728da5ee51e787a0
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-27 13:19:33 +0200

    refactor: add schedule to ticket

--- src/models/Ticket.tsx
+++ src/models/Ticket.tsx
@@ -1,9 +1,10 @@
 export type Ticket = {
     date: string,
     room: number,
-    seat: string,
+    seats: string[],
     movie: string,
     image: string,
     qrCode: string,
+    schedule: string,
     orientation?: "horizontal" | "vertical"
 }
