0d5d42c9ada10b636a199749cd1a2d5bc1102f89
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-27 15:23:17 +0200

    feat: add cast section

---
+++ src/components/organisms/Cast.tsx
@@ -0,0 +1,20 @@
+import { CastMember } from "../atoms/CastMember"
+
+type Cast =  {
+    cast : CastMember[]
+}
+export const Cast = ({cast}: Cast) => {
+    return (
+        <div className="flex flex-col items-start justify-center">
+            <span className="text-3xl leading-9 font-semibold text-white">Cast</span>
+            <div className="flex items-start gap-4 self-stretch overflow-auto">
+                {cast.map((member) => {
+                    return (
+                        <CastMember key={member.name} image={member.image} name={member.name} role={member.role}/>
+                    )
+                })}
+            </div>
+
+        </div>
+    )
+}
