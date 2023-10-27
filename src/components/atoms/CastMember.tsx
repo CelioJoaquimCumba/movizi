5205ae0849220d956f5af1b342e8af402dbc515d
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-27 15:23:08 +0200

    refactor: add no wrap

--- src/components/atoms/CastMember.tsx
+++ src/components/atoms/CastMember.tsx
@@ -1,4 +1,4 @@
-type CastMember = {
+export type CastMember = {
     image: string,
     name: string,
     role: string
@@ -8,8 +8,8 @@ export const CastMember = ({image, name, role}: CastMember) => {
     return(
         <div className="flex p-2 flex-col items-center gap-2 rounded-md">
             <img className="w-24 rounded-full aspect-square object-cover" src={image} alt={name}/>
-            <span className="text-white text-base leading-6 font-normal">{role}</span>
-            <span className="text-sm leading-5 font-normal text-gray-300">{name}</span>
+            <span className="text-white text-base leading-6 font-normal whitespace-nowrap">{role}</span>
+            <span className="text-sm leading-5 font-normal text-gray-300 whitespace-nowrap">{name}</span>
         </div>
     )
 }
