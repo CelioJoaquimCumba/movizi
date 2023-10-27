9cba4f1744cda8ab69c9c4f4ded2e4137f4a1775
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-27 16:29:31 +0200

    refactor: add noWrap for price

--- src/components/atoms/BookingItem.tsx
+++ src/components/atoms/BookingItem.tsx
@@ -6,8 +6,8 @@ export type BookingItem = {
 export const BookingItem = ({item, price, quantity}: BookingItem) => {
     return (
         <div className="flex py-4 px-2 justify-between items-center rounded w-full">
-            <span className="text-base leading-6 font-normal text-white">{item}</span>
-            <span className="text-sm leading-5 font-normal text-white">{ quantity>1 && `${quantity} X`}  {price} $ </span>
+            <span className="text-base leading-6 font-normal text-white ">{item}</span>
+            <span className="text-sm leading-5 font-normal text-white whitespace-nowrap">{ quantity>1 && `${quantity} X`}  {price} $ </span>
         </div>
     )
 }
