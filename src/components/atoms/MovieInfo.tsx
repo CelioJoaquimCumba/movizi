52f5e8708a3d66db601b991fcbcb3e80614d9c2d
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-27 13:20:36 +0200

    refactor: change movie info interface

--- src/components/atoms/MovieInfo.tsx
+++ src/components/atoms/MovieInfo.tsx
@@ -1,3 +1,5 @@
+import { Ticket } from "../../models/Ticket"
+
 type MovieInfo = {
     date: string,
     schedule: string,
@@ -6,13 +8,13 @@ type MovieInfo = {
     image: string
 }
 
-export const MovieInfo = ({date, schedule, title, seats, image}: MovieInfo) => {
+export const MovieInfo = ({date, schedule, movie, seats, image}: Ticket) => {
     return(
-        <div className="flex p-4 justify-center items-start gap-2 rounded-xl bg-slate-900 hover:bg-slate-800 w-full">
-            <img className="rounded-xl object-cover w-32 aspect-[3/4]" src={image} alt={`${title} ticket`}/>
+        <div className="flex p-4 justify-center items-start gap-4 rounded-xl bg-slate-900 hover:bg-slate-800 w-full">
+            <img className="rounded-xl object-cover w-32 aspect-[3/4]" src={image} alt={`${movie} ticket`}/>
             <div className="flex flex-col items-start gap-4 flex-grow">
-                <span className="text-sm leading-5 font-semibold text-white self-stretch whitespace-nowrap">{title}</span>
-                <span className="text-sm leading-5 font-semibold text-white self-stretch whitespace-nowrap">{seats} seats</span>
+                <span className="text-sm leading-5 font-semibold text-white self-stretch whitespace-nowrap">{movie}</span>
+                <span className="text-sm leading-5 font-semibold text-white self-stretch whitespace-nowrap">{seats.map(seat=>seat)} seats</span>
                 <span className="text-sm leading-5 font-semibold text-white self-stretch whitespace-nowrap">Date: {date}</span>
                 <span className="text-sm leading-5 font-semibold text-white self-stretch whitespace-nowrap">Schedule: {schedule}</span>
             </div>
