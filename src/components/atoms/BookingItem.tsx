a1f88b4393edf1db4f1bc07c43e14aada87b8bd3
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-27 16:26:06 +0200

    refactor: add possibility of not having a quantity and increase with to full

--- src/components/atoms/BookingItem.tsx
+++ src/components/atoms/BookingItem.tsx
@@ -1,13 +1,13 @@
-type BookingItem = {
+export type BookingItem = {
     item: string,
     price: number,
     quantity: number
 }
 export const BookingItem = ({item, price, quantity}: BookingItem) => {
     return (
-        <div className="flex py-4 px-2 justify-between items-center rounded">
+        <div className="flex py-4 px-2 justify-between items-center rounded w-full">
             <span className="text-base leading-6 font-normal text-white">{item}</span>
-            <span className="text-sm leading-5 font-normal text-white">{quantity} x {price} $ </span>
+            <span className="text-sm leading-5 font-normal text-white">{ quantity>1 && `${quantity} X`}  {price} $ </span>
         </div>
     )
 }
