c4a85a777527666f6c1d74b16fa7edc09176dc18
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-10-28 18:48:58 +0200

    feat: add form progress

--- src/components/molecules/LocationTrack.tsx
+++ src/components/molecules/LocationTrack.tsx
@@ -1,11 +1,11 @@
 import { FormLocation } from "../atoms/FormLocation"
 
 type LocationTrackProps = {
-    index: number
+    index: 0|1|2
 }
 export const LocationTrack = ({index}:LocationTrackProps) => {
     return (
-        <div className="flex px-8 items-start gap-4 rounded-2xl">
+        <div className="flex px-8 items-start gap-4 rounded-2xl w-full justify-center">
             <FormLocation selected={index === 0} label={"Details"}/>
             <FormLocation selected={index === 1} label={"Booking"}/>
             <FormLocation selected={index === 2} label={"Payment"}/>
---
+++ src/components/organisms/FormProgress.tsx
@@ -0,0 +1,14 @@
+import { LocationTrack } from "../molecules/LocationTrack"
+type FormProgress = {
+    index: 0|1|2
+}
+export const FormProgress = ({index}:FormProgress) => {
+    return (
+        <div className="flex justify-between items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                <path d="M13.3333 25.3334L4 16M4 16L13.3333 6.66669M4 16H28" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <LocationTrack index={index}/>
+        </div>
+    )
+}
