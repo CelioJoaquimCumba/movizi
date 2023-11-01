e596b1aca151990ad7a829230921d48772c719bc
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-01 12:00:53 +0200

    refactor: make full width

--- src/components/organisms/FormProgress.tsx
+++ src/components/organisms/FormProgress.tsx
@@ -4,7 +4,7 @@ type FormProgress = {
 }
 export const FormProgress = ({index}:FormProgress) => {
     return (
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center w-full">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                 <path d="M13.3333 25.3334L4 16M4 16L13.3333 6.66669M4 16H28" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
