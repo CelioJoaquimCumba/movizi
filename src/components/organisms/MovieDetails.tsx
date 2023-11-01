070a99ab8dbe4ed56061da9e122522ff443327aa
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-01 14:10:37 +0200

    refactor: change image aspect of tablet

--- src/components/organisms/MovieDetails.tsx
+++ src/components/organisms/MovieDetails.tsx
@@ -3,13 +3,14 @@ import { Rating } from "../atoms/Rating"
 
 export const MovieDetails = ({title, image, rating, genre, duration, language, description, directors, caption}: Movie) => {
     return(
-        <div className="flex justify-start items-start gap-2 w-full">
-            <img className="w-64 aspect-[3/4] rounded-2xl" src={image.portrait} alt={`${title} poster`}/>
+        <div className="flex flex-col lg:flex-row justify-start items-start gap-2 w-full">
+            <img className="w-64 hidden lg:block aspect-[3/4] rounded-2xl" src={image.portrait} alt={`${title} poster`}/>
             <div className="flex flex-col items-center gap-4 flex-grow">
                 <div className="flex items-start gap-2 self-stretch">
                     <span className="text-3xl leading-0 font-semibold text-white flex-grow">{title}</span>
                     <Rating rating={rating ? rating : 0}/>
                 </div>
+            <img className="w-full lg:hidden aspect-[4/3] rounded-2xl" src={image.landscape} alt={`${title} poster`}/>
                 <span className="text-base leading-6 font-normal text-white w-full ">{genre} | {duration}</span>
                 <span className="text-base leading-6 font-normal text-white w-full">{description}</span>
                 <span className="text-base leading-6 font-normal text-white w-full">Directed by: {directors}</span>
