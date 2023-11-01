e607a0f2cccdd4fd0de303b8299427adcd6eaaf3
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-01 12:19:41 +0200

    refactor: change width to full

--- src/components/organisms/Schedule.tsx
+++ src/components/organisms/Schedule.tsx
@@ -2,7 +2,7 @@ import { SchedulePicker } from "../molecules/SchedulePicker"
 
 export const Schedule = ({schedules} : SchedulePicker) => {
     return(
-        <div className="flex flex-col items-start gap-2">
+        <div className="flex flex-col items-start gap-2 w-full">
             <span className="text-xl leading-7 font-semibold text-white">Schedule</span>
             <SchedulePicker schedules={schedules}/>
 
