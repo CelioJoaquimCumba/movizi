dfb03d428488ad10f6b7f0c522ab69df1d4924e9
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-01 12:02:42 +0200

    refactor: add full width

--- src/components/organisms/Cast.tsx
+++ src/components/organisms/Cast.tsx
@@ -5,7 +5,7 @@ type Cast =  {
 }
 export const Cast = ({cast}: Cast) => {
     return (
-        <div className="flex flex-col items-start justify-center">
+        <div className="flex flex-col items-start justify-center w-full">
             <span className="text-3xl leading-9 font-semibold text-white">Cast</span>
             <div className="flex items-start gap-4 self-stretch overflow-auto">
                 {cast.map((member) => {
