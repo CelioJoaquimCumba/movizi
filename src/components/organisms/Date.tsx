f94fd9e85fe35233e3df82f7e24e8421281d3a2e
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-01 12:15:24 +0200

    refactor: add full width

--- src/components/organisms/Date.tsx
+++ src/components/organisms/Date.tsx
@@ -3,7 +3,7 @@ import { DatePicker } from "../molecules/DatePicker"
 
 export const Date = ({startDate, endDate} : DatePicker) => {
     return(
-        <div className="flex flex-col items-start gap-4">
+        <div className="flex flex-col items-start gap-4 w-full">
             <span className="text-xl leading-7 font-semibold text-white">Date</span>
             <DatePicker startDate={startDate} endDate={endDate}/>
         </div>
