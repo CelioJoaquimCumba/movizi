e83e93740778fbd9ec667283a5a02719f409f988
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-02 07:13:19 +0200

    refactor: add horizontal padding

--- src/components/organisms/Tickets.tsx
+++ src/components/organisms/Tickets.tsx
@@ -8,7 +8,7 @@ type Tickets = {
 
 export const Tickets = ({heading, tickets}: Tickets) => {
     return(
-        <div className="flex flex-col gap-2 items-start self-stretch w-full">
+        <div className="flex flex-col px-4 gap-2 items-start self-stretch w-full">
             <h1 className="text-xl leading-7 font-semibold text-white">{heading}</h1>
             {tickets.map((ticket) => {
                 return(
