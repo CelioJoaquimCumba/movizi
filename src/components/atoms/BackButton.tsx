2116f5066df11a442b21b594a93e444a28dd27c5
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-02 08:25:18 +0200

    refactor: rectify typos on svg

--- src/components/atoms/BackButton.tsx
+++ src/components/atoms/BackButton.tsx
@@ -4,7 +4,7 @@ export const BackButton = ({...props}) => {
     return (
         <button className="flex p-1 items-start gap-2 rounded-full hover:bg-white hover:bg-opacity-10" {...props}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                <path d="M13.3333 25.3334L4 16M4 16L13.3333 6.66669M4 16H28" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M13.3333 25.3334L4 16M4 16L13.3333 6.66669M4 16H28" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
         </button>
     )
