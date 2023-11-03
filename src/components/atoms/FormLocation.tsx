ff20c7800353001783cd1757ba9908f44a902a39
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-03 09:16:36 +0200

    refactor: change form location appearance

--- src/components/atoms/FormLocation.tsx
+++ src/components/atoms/FormLocation.tsx
@@ -5,16 +5,9 @@ type FormLocation = {
 
 export const FormLocation = ({selected, label}: FormLocation) =>{
     return(
-        <div className="flex flex-col items-center gap-2">
-            {selected ?
-                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none">
-                    <circle cx="8.5" cy="8" r="6" fill="#0F172A" stroke="white" strokeWidth="4"/>
-                </svg>
-            :
-                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none">
-                    <circle cx="8.5" cy="8" r="8" fill="white"/>
-                </svg>
-            }
+        <div className="flex flex-col items-center gap-1">
+                <div className={`flex w-8 h-2 ${ selected ?"bg-slate-900" :"bg-white"} border-2 border-white rounded`}>
+                </div>
             <span className="text-xs leading-4 font-normal text-center text-white">{label}</span>
         </div>
     )
