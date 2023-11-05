09255b0e481a5af6168bebccd76e934248460afb
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-05 10:35:53 +0200

    refactor: change path organization

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,63 +1,69 @@
-import {createBrowserRouter} from 'react-router-dom'
-import { Login } from '../pages/Login';
-import { Home } from '../pages/Home';
-import { MovieDetails } from '../pages/MovieDetails';
-import { Booking } from '../pages/Booking';
-import { Payment } from '../pages/Payment';
-import { Register } from '../pages/Register';
-import { PaymentSuccess } from '../pages/Payment-Success';
-import { PaymentFailure } from '../pages/PaymentFailure';
-import { Tickets } from '../pages/Tickets';
-import { TicketDetails } from '../pages/TicketDetails';
+import { createBrowserRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        children: [
-            {
-                path: "login",
-                element: <Login/>
-            },
-            {
-                path: "register",
-                element: <Register/>
-            },
-            {
-                path: "",
-                element: <Home/>
-            },
-            {
-                path: "movie/:id",
-                element: <MovieDetails/>
-            },
-            {
-                path:"booking/:id",
-                element: <Booking/>
-            },
-            {
-                path: "payment",
-                element: <Payment/>
-            },
-            {
-                path: "payment-success",
-                element: <PaymentSuccess/>
-            },
-            {
-                path: "payment-failure",
-                element: <PaymentFailure/>
-            },
-            {
-                path: "tickets",
-                element: <Tickets/>
-            },
-            {
-                path: "ticket/:id",
-                element: <TicketDetails/>
-            },
-        ]
-    },
-    {
-        path: "/*",
-        element: <div>404</div>
-    }
-])
+// Import your page components
+import { 
+  Login, 
+  Home, 
+  MovieDetails, 
+  Booking, 
+  Payment, 
+  Register, 
+  PaymentSuccess, 
+  PaymentFailure, 
+  Tickets, 
+  TicketDetails 
+} from '../pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'movie/:id',
+        element: <MovieDetails />
+      },
+      {
+        path: 'booking/:id',
+        element: <Booking />
+      },
+      {
+        path: 'payment',
+        element: <Payment />
+      },
+      {
+        path: 'payment-success',
+        element: <PaymentSuccess />
+      },
+      {
+        path: 'payment-failure',
+        element: <PaymentFailure />
+      },
+      {
+        path: 'tickets',
+        element: <Tickets />
+      },
+      {
+        path: 'ticket/:id',
+        element: <TicketDetails />
+      },
+    ]
+  },
+  {
+    path: '/*',
+    element: <div>404</div>
+  }
+]);
+
+export { router };
