487c5f472ee0fee664118b9bc31347b4a2ac5b47
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-05 12:40:12 +0200

    refactor: resolve key problem

--- src/components/molecules/Seats.tsx
+++ src/components/molecules/Seats.tsx
@@ -15,7 +15,7 @@ export const Seats = ({row, column, start, sold, updateSeats}: Seats) => {
         const array = []
         for (let j = 0 ; j < column ; j ++ ) {
             if ( sold && sold.some(obj => obj.column -1 === j && obj.row-1 === i)) {
-                array.push("0")
+                array.push("0"+i+j)
             } else {
                 array.push(alphabet[i]+(j+start))
             }
@@ -31,7 +31,7 @@ export const Seats = ({row, column, start, sold, updateSeats}: Seats) => {
                         <div key={index} className="flex flex-start gap-1">
                             {array.map(
                                 (seat)=> 
-                                <Seat updateSeats={updateSeats} key={seat} seat={seat.toString()} status={ seat === "0" ? "occupied" : "available"} />
+                                <Seat updateSeats={updateSeats} key={seat} seat={seat.toString()} status={ seat.at(0) === "0" ? "occupied" : "available"} />
                             )}
                         </div>
                     )
