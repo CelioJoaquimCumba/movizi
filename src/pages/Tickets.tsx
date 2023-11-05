6a8390850bbfafd8b9efa8759ed156b670a376cb
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-05 15:20:36 +0200

    feat: read firebase tickets

--- src/pages/Tickets.tsx
+++ src/pages/Tickets.tsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import { Footer } from "../components/molecules/Footer";
 import { NavBar } from "../components/molecules/NavBar"
 import { Tickets as MovieTicket } from "../components/organisms/Tickets"
 import { Ticket } from "../models/Ticket";
+import { useAuth } from "../firebase/auth";
+import { useEffect, useState } from "react";
+import { getTickets } from "../firebase/firestore";
 
 
 const movieTickets: Ticket[] = [
@@ -26,12 +30,31 @@ const movieTickets: Ticket[] = [
     // Add more ticket objects as needed
 ];
 export const Tickets = () => {
+    const { authUser, isLoading } = useAuth()
+    const [tickets, setTickets] = useState<Array<Ticket>>([{uid:"",id:"",date:"",room:0,seats:[],movie:"",image:"",qrCode:"",schedule:""}])
+    const navigate = useNavigate()
+    if(!isLoading && !authUser){
+        navigate("/login")
+    }
+    useEffect(()=>{
+        async function fetchData() {
+            // You can await here
+            if(authUser){
+                const tickets = await getTickets(authUser.uid)
+                setTickets(tickets)
+                console.log(tickets)
+            }
+            // ...
+        }
+        console.log(authUser)
+        fetchData()
+    },[authUser])
     return(
         <div className="flex flex-col items-start gap-2 w-full h-full min-h-screen bg-black">
             <NavBar/>
             <div className="flex flex-col items-start gap-2 flex-grow self-stretch md:py-16 md:px-24">
-                <MovieTicket heading={"Purchased Tickets"} tickets={movieTickets}/>
-                <MovieTicket heading={"History"} tickets={movieTickets}/>
+                <MovieTicket heading={"Purchased Tickets"} tickets={tickets.filter(t=>!t.expired)}/>
+                <MovieTicket heading={"History"} tickets={tickets.filter(t=>t.expired)}/>
             </div>
             <Footer/>
         </div>
