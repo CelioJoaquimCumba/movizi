506df540a88443a9b8e562647f8de5a2a5254ee5
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-05 16:32:43 +0200

    refactor: remove form tracker

--- src/components/organisms/FormProgress.tsx
+++ src/components/organisms/FormProgress.tsx
@@ -8,7 +8,7 @@ export const FormProgress = ({index}:FormProgress) => {
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                 <path d="M13.3333 25.3334L4 16M4 16L13.3333 6.66669M4 16H28" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-            <LocationTrack index={index}/>
+            {/* <LocationTrack index={index}/> */}
         </div>
     )
 }
