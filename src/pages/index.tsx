faf07085ea942b01d07f768fc43d19b93a8e6917
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-05 10:35:29 +0200

    refactor: add a pages index file

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export { Payment } from './Payment';
+export { PaymentSuccess } from './Payment-Success';
+export { TicketDetails } from './TicketDetails';
+export { Tickets } from './Tickets';
+export { MovieDetails } from './MovieDetails';
+export { Login } from './Login';
+export { Register } from './Register';
+export { Home } from './Home';
+export { Booking } from './Booking';
+export { PaymentFailure } from './PaymentFailure'
+
