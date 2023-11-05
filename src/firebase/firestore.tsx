b0e8e1e9c39ba5ddded271cd5deba186ecfd245e
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-05 15:19:27 +0200

    feat: add tickets to firebase

--- src/firebase/firestore.tsx
+++ src/firebase/firestore.tsx
@@ -1,4 +1,4 @@
-import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, setDoc, where } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, setDoc, where } from "firebase/firestore";
 import { db } from "./firebase"
 import { getDownloadURL } from "firebase/storage";
 import { Ticket } from "../models/Ticket";
@@ -6,4 +6,27 @@ import { Ticket } from "../models/Ticket";
 const TICKETS_COLLECTION = "tickets"
 export const addTicket = ({id, date, room, seats, movie, image, qrCode, schedule}:Ticket ) => {
     addDoc(collection(db, TICKETS_COLLECTION), {id, date, room, seats, movie, image, qrCode, schedule})
+}
+export const getTickets = async (uid:string):Promise<Ticket[]> => {
+    const tickets = query(collection(db,TICKETS_COLLECTION),where("id","==",uid))
+    const querySnapshot = getDocs(tickets)
+
+    const allTickets:Ticket[] = []
+    for (const documentSnapshot of (await querySnapshot).docs) {
+        const ticket = documentSnapshot.data()
+        allTickets.push({
+            ...ticket,
+            id: documentSnapshot.id,
+            movie: ticket.movie,
+            image: ticket.image,
+            qrCode: ticket.qrCode,
+            schedule: ticket.schedule,
+            seats: ticket.seats,
+            date: ticket.date,
+            room: ticket.room,
+            uid: ticket.id
+        })
+    }
+    return allTickets
+
 }
