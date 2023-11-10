92abd9f0049424e48c5b0ac4e4d14a0ea226854f
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-11-10 17:16:24 +0200

    refactor: add ranking to the movies top

--- src/components/organisms/TopMovies.tsx
+++ src/components/organisms/TopMovies.tsx
@@ -10,20 +10,20 @@ export const TopMovies = ({movies}:TopMovies) => {
         <div className="flex flex-col items-start gap-2 w-full">
             <h1 className="text-xl leading-7 font-semibold text-white">Top Movies This Week</h1>
             <div className=" hidden md:flex flex-row items-start gap-2 self-stretch ">
-                <CardMovie type="rank" orientation="vertical" {...movies[0]}/>
+                <CardMovie type="rank" orientation="vertical" {...movies[0]} ranking={1}/>
                 <div className="flex flex-col items-start gap-2  self-stretch w-full">
-                    {movies.length>1 && <CardMovie type="rank" orientation="horizontal" {...movies[1]}/>}
-                    {movies.length>2 && <CardMovie type="rank" orientation="horizontal" {...movies[2]}/>}
+                    {movies.length>1 && <CardMovie type="rank" orientation="horizontal" {...movies[1]} ranking={2}/>}
+                    {movies.length>2 && <CardMovie type="rank" orientation="horizontal" {...movies[2]} ranking={3}/>}
                 </div>
                 <div className="flex flex-col items-start gap-2 self-stretch w-full">
-                    {movies.length > 3 && <CardMovie type="rank" orientation="horizontal" {...movies[3]}/>}
-                    {movies.length > 4 && <CardMovie type="rank" orientation="horizontal" {...movies[4]}/>}
+                    {movies.length > 3 && <CardMovie type="rank" orientation="horizontal" {...movies[3]} ranking={4}/>}
+                    {movies.length > 4 && <CardMovie type="rank" orientation="horizontal" {...movies[4]} ranking={5}/>}
                 </div>
             </div>
             <div className="flex flex-col items-start gap-2 self-stretch md:hidden">
-                {movies.filter((movie, index) => index < 3 && movie).map((movie) => {
+                {movies.filter((movie, index) => index < 3 && movie).map((movie, index) => {
                     return(
-                        <MovieChartItem key={movie.title} id={movie.id} ranking={movie.ranking ? movie.ranking : 0} title={movie.title} image={movie.image.portrait} bookings={movie.bookings ? movie.bookings : 0}/>
+                        <MovieChartItem key={movie.title} id={movie.id} ranking={index+1} title={movie.title} image={movie.image.portrait} bookings={movie.bookings ? movie.bookings : 0}/>
                     )
                 })}
 
